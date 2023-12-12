e45049716487ab7156f5779d0276251b1be1840c
MostafaElebzary <mostafaelebzary@gmail.com>
2023-12-12 10:27:43 +0200

    m

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreAttendanceRequest;
+use App\Http\Requests\UserAttendanceReportRequest;
 use App\Http\Requests\UserResetDeviceIdRequest;
 use App\Http\Requests\UsersRequest;
 use App\Http\Requests\UsersUpdateRequest;
@@ -28,14 +29,14 @@ class AttendanceController extends Controller
         $user = check_jwt($jwt);
         if ($user) {
             $data['user_id'] = $user->id;
-            $data['note'] = $request->note;
+            $data['notes'] = $request->note;
             $data['type'] = $request->type;
             $data['date'] = Carbon::now()->format("Y-m-d");
             $attendance_today = Attendance::where('user_id', $user->id)
                 ->where('date', $data['date'])
                 ->first();
             if ($request->type == "check_in") {
-                if ($attendance_today && $attendance_today->in_time != null) {
+                if ($attendance_today) {
                     return msgdata(failed(), ' تم تسجيل حضور اليوم من قبل ', (object)[]);
                 }
                 $data['in_time'] = Carbon::now()->format("H:i");
@@ -51,15 +52,20 @@ class AttendanceController extends Controller
                     return msgdata(failed(), ' تم تسجيل انصراف اليوم من قبل', (object)[]);
                 }
 
-
                 $data['out_time'] = Carbon::now()->format("H:i");
-                Attendance::create($data);
+                $attendance_today->update($data);
                 return msgdata(success(), 'تم تسجيل انصراف بنجاح', (object)[]);
             }
 
         } else {
             return msgdata(not_authoize(), 'برجاء تسجيل الدخول', (object)[]);
         }
+    }
+
+    public function attendanceReport(UserAttendanceReportRequest $request){
+
+        
+
     }
 
 }
